235f962cb82a7bd293d5b53d0f07aaaa45c44df9
mbodenham <matthew@bodenham.me>
2020-07-21 17:20:24 +0100

    Show sparsity

--- CPD/__init__.py
+++ CPD/__init__.py
@@ -55,13 +55,13 @@ class CPD(nn.Module):
         detection = self.agg2(x5_2, x4_2, x3_2)
 
         return self.upsample(attention), self.upsample(detection)
-        
+
 
 class CPD_darknet19(nn.Module):
     def __init__(self, channel=32):
         super(CPD_darknet19, self).__init__()
         self.name = 'CPD_darknet19'
-        self.vgg = Darknet19()
+        self.darknet = Darknet19()
         self.rfb3_1 = RFB(128, channel)
         self.rfb4_1 = RFB(256, channel)
         self.rfb5_1 = RFB(512, channel)
@@ -74,9 +74,9 @@ class CPD_darknet19(nn.Module):
 
         self.HA = HA()
         self.upsample = nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False)
-        modules = [self.vgg, self.rfb3_1, self.rfb4_1, self.rfb5_1, self.agg1,
+        modules = [self.darknet, self.rfb3_1, self.rfb4_1, self.rfb5_1, self.agg1,
                    self.rfb3_2, self.rfb4_2, self.rfb5_2, self.agg2, self.HA, self.upsample]
-        modules_names = ['vgg', 'rfb3_1', 'rfb4_1', 'rfb5_1', 'agg1',
+        modules_names = ['darknet', 'rfb3_1', 'rfb4_1', 'rfb5_1', 'agg1',
                    'rfb3_2', 'rfb4_2', 'rfb5_2', 'agg2', 'HA', 'upsample']
         print('Parameters')
         for module, name in zip(modules, modules_names):
@@ -84,21 +84,21 @@ class CPD_darknet19(nn.Module):
             print('{}\t{}'.format(name, params))
 
     def forward(self, x):
-        x1 = self.vgg.conv1(x)
-        x2 = self.vgg.conv2(x1)
-        x3 = self.vgg.conv3(x2)
+        x1 = self.darknet.conv1(x)
+        x2 = self.darknet.conv2(x1)
+        x3 = self.darknet.conv3(x2)
 
         x3_1 = x3
-        x4_1 = self.vgg.conv4_1(x3_1)
-        x5_1 = self.vgg.conv5_1(x4_1)
+        x4_1 = self.darknet.conv4_1(x3_1)
+        x5_1 = self.darknet.conv5_1(x4_1)
         x3_1 = self.rfb3_1(x3_1)
         x4_1 = self.rfb4_1(x4_1)
         x5_1 = self.rfb5_1(x5_1)
         attention = self.agg1(x5_1, x4_1, x3_1)
 
         x3_2 = self.HA(attention.sigmoid(), x3)
-        x4_2 = self.vgg.conv4_2(x3_2)
-        x5_2 = self.vgg.conv5_2(x4_2)
+        x4_2 = self.darknet.conv4_2(x3_2)
+        x5_2 = self.darknet.conv5_2(x4_2)
         x3_2 = self.rfb3_2(x3_2)
         x4_2 = self.rfb4_2(x4_2)
         x5_2 = self.rfb5_2(x5_2)
--- prune_d.py
+++ prune_d.py
@@ -7,4 +7,18 @@ device = torch.device('cuda')
 state_dict = torch.load('CPD_darknet19.pth', map_location=torch.device(device))
 model = CPD().to(device)
 
- distiller.model_summary(model, 'sparsity')
+df = distiller.weights_sparsity_summary(model, True)
+print(df)
+
+t, total = distiller.weights_sparsity_tbl_summary(model, True)
+print(t)
+print(total)
+
+#convs = []
+#for name, module in model.named_modules():
+#	if len(module._modules) == 0:
+#		if module.__class__.__name__ == 'Conv2d':
+#			convs.append([name, module.__class__.__name__])
+#			print(convs[-1])
+#
+#print(len(convs))
